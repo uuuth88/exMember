8931085533157c4ea0fc13214ff19ee2837c1642
uuuth <xoghd88@gmail.com>
2019-06-22 00:54:02 +0900

    6-1. 지금까지 써온 쿼리문 저장

--- WebContent/doc/query.sql
+++ WebContent/doc/query.sql
@@ -1,4 +1,22 @@
 --계정 생성
 CREATE USER exLogin IDENTIFIED BY exLogin;
+
 --계정에 권한 부여
 GRANT connect, dba TO exLogin;
+
+--테이블 생성
+CREATE TABLE memberInfo(
+   id varchar2(50) not null,
+   pw varchar2(100) not null,
+   name varchar2(100) not null,
+   nickname varchar2(100)
+);
+
+--기본키 제약 부여
+ALTER TABLE memberInfo ADD CONSTRAINT pk_member PRIMARY KEY(id);
+
+--테이블 제거
+DROP TABLE memberInfo CASCADE CONSTRAINT;
+
+--회원 정보 등록하는 쿼리
+INSERT INTO memberInfo(id, pw, name, nickname) values(?, ?, ?, ?);﻿
