a475b22d180b9ee685d10d395f9ebfec8f6ce4c8
uuuth <xoghd88@gmail.com>
2019-06-22 00:32:13 +0900

    6. JoinServlet 작성 트라이:01

--- src/com/ex/bbs/JoinServlet.java
+++ src/com/ex/bbs/JoinServlet.java
@@ -1,41 +1,38 @@
 package com.ex.bbs;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class JoinServlet
- */
 @WebServlet("/join")
 public class JoinServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public JoinServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//1. join.jsp에서 넘어온 데이터들을 알맞은 변수에 담는다.
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		String nick = request.getParameter("nickname");
+
+		//2. MemberVO 객체를 생성해 저장한다.
+		MemberVO vo = new MemberVO();
+		vo.setId(id);
+		vo.setPw(pw);
+		vo.setName(name);
+		vo.setNick(nick);
+		
+		//3. MemberDAO 객체를 생성한 후 add()메소드를 호출해
+		//회원 정보를 저장한다.
+		MemberDAO dao = new MemberDAO();
+		dao.add(vo);
+		
+		//4. 작업을 마치면 joinSuccess.jsp로 포워딩.
+		RequestDispatcher rd = request.getRequestDispatcher("joinSuccess.jsp");
+		rd.forward(request, response);
 	}
 
 }
