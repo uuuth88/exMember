c2db9c7c3741819bcfad8d29c730af13ef6bb732
uuuth <xoghd88@gmail.com>
2019-06-22 00:22:27 +0900

    5. join.jsp action값 변경 MemberDAO 작성

--- src/com/ex/bbs/JoinServlet.java
+++ src/com/ex/bbs/JoinServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class JoinServlet
  */
-@WebServlet("/JoinServlet")
+@WebServlet("/join")
 public class JoinServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
