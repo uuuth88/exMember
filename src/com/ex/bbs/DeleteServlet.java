9f2780aa910b25c93ebf962ed5bf79a39bc3a2df
uuuth <xoghd88@gmail.com>
2019-06-26 23:15:26 +0900

    13. 삭제기능까지 완성

--- src/com/ex/bbs/DeleteServlet.java
+++ src/com/ex/bbs/DeleteServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.*;
 
 @WebServlet("/delete")
 public class DeleteServlet extends HttpServlet {
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		
 //		세션에 담은 로그인한 회원 정보를 가져온다		
@@ -18,7 +18,7 @@ public class DeleteServlet extends HttpServlet {
 		
 //		회원의 id를 변수에 담은 후		
 		String id = member.getId();
-
+		
 //		dao.delete()메소드의 인자값으로 활용한다		
 		MemberDAO dao = new MemberDAO();
 		dao.delete(id);
@@ -26,5 +26,14 @@ public class DeleteServlet extends HttpServlet {
 //		모든 작업을 마친 후 deleteDone페이지로 포워딩		
 		RequestDispatcher rd = request.getRequestDispatcher("deleteDone.jsp");
 		rd.forward(request, response);
+		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doDelete(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doDelete(request, response);
 	}
 }
